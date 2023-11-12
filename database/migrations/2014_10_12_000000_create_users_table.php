72e6a633e2c1f94f0e4d0bef79c517f7a223174c
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-11-12 23:52:51 +0700

    Menghilangkan timestramp pada users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('password');
             $table->enum('role', ['member', 'admin'])->default('member');
             $table->boolean('isAllowed')->default(true);
-            $table->timestamps();
         });
     }
 
