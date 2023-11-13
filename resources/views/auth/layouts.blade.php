f382bcb8b9e7594523b8c63db5bb46fce1c75862
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-11-13 22:35:58 +0700

    + Ganti color gradient supaya ga bosen

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
         body {
-            background: linear-gradient(-45deg,  #5C27FE,#C165DD,#1de5e2,#2AFEB7);
+            background: linear-gradient(-45deg,  #0093E9, #80D0C7);
 	        background-size: 400% 400%;
 	        animation: gradient 15s ease infinite;
         }
