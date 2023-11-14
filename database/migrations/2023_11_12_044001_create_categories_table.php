b0a069332a8afaa2185081768c51b1ec67629fd7
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-11-14 12:39:47 +0700

    + Urutan Pembuatan Migrasi agak salah

--- database/migrations/2023_11_12_044001_create_categories_table.php
+++ database/migrations/2023_11_12_044001_create_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id('id');
+            $table->id('category_id')->primary();
             $table->string('category_name');
         });
     }
