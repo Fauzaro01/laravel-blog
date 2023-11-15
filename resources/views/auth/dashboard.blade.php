6fcbd71f4fd5c6ca83d67539492240ac1a2e7962
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-11-15 12:01:14 +0700

    + Debuging Khusus Admin dan ada tabel blog client

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -16,7 +16,31 @@
                         You are logged in!
                     </div>       
                 @endif       
-                {{ Auth::user() }}
+
+                @if ( Auth::user()->role == "admin" )
+                    {{ Auth::user() }}
+                    <br>
+                @endif
+
+                <a href="{{ route('blog.addblog') }}" class="btn btn-outline-dark"> Tambah Blog [ + ]</a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Content</th>
+                            <th>Category</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>    
