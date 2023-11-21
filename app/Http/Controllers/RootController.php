9d6425191439236d392da1a5cb86f2e6d83a9ec7
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-11-21 09:51:20 +0700

    + Memberikan Data Category dan buat function

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -4,44 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Posts;
+use App\Models\Categories;
 
 class RootController extends Controller
 { 
-    public function root (Request $req) {
-        function pisahkanGanjilGenap($array) {
-            $barisGanjil = [];
-            $barisGenap = [];
-        
-            foreach ($array as $key => $baris) {
-                if ($key % 2 == 0) {
-                    // Baris ganjil
-                    $barisGanjil[] = $baris;
-                } else {
-                    // Baris genap
-                    $barisGenap[] = $baris;
-                }
+    public static function pisahkanGanjilGenap($array) {
+        $barisGanjil = [];
+        $barisGenap = [];
+    
+        foreach ($array as $key => $baris) {
+            if ($key % 2 == 0) {
+                // Baris ganjil
+                $barisGanjil[] = $baris;
+            } else {
+                // Baris genap
+                $barisGenap[] = $baris;
             }
-        
-            return [
-                'ganjil' => $barisGanjil,
-                'genap' => $barisGenap,
-            ];
         }
+    
+        return [
+            'ganjil' => $barisGanjil,
+            'genap' => $barisGenap,
+        ];
+    }
 
+    public function root (Request $req) {
         if($req->query('find') != null) {
-            $posts = Posts::where('title', 'LIKE', "%$$req->query('find')*%")->get();
+            $posts = Posts::where('title', 'LIKE', "%" . $req->query('find') . "%")->get();
         } else {
             $posts = Posts::all();
         }
         
-        $blogs = pisahkanGanjilGenap($posts);
-        return view("index", compact("blogs"));
+        $categories = self::pisahkanGanjilGenap(Categories::all());
+        $blogs = self::pisahkanGanjilGenap($posts);
+        return view("index", compact("blogs", "categories"));
     }
-
+    
     public function showblog($id) {
         try {
+            $categories = self::pisahkanGanjilGenap(Categories::all());
             $posts = Posts::findOrFail($id);
-            return view('blog.index', compact('posts'));
+            return view('blog.index', compact('posts', "categories"));
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             return redirect()->route('home');
         }
